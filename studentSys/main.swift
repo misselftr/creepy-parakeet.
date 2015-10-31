3e8ad4b40f92a50ef0c2b9ea7e0bbb196b52aa6e
ÖZLEM ATABAY <ozlematabay@OZLEM-MacBook-Pro.local>
2015-10-31 17:08:09 +0300

    Initial Commit

---
+++ studentSys/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  studentSys
+//
+//  Created by ÖZLEM ATABAY on 31.10.2015.
+//  Copyright (c) 2015 ÖZLEM ATABAY. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
