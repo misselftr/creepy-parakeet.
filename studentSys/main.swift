66f600699c871c3039a5306f735d901876a609c7
ÖZLEM ATABAY <ozlematabay@OZLEM-MacBook-Pro.local>
2015-10-31 19:11:47 +0300

    Student and Girl class structure

--- studentSys/main.swift
+++ studentSys/main.swift
@@ -1,12 +1,85 @@
-//
-//  main.swift
-//  studentSys
-//
-//  Created by ÖZLEM ATABAY on 31.10.2015.
-//  Copyright (c) 2015 ÖZLEM ATABAY. All rights reserved.
-//
+let GENDER = ["female","male"]
 
-import Foundation
+class Student {
+    var name: String = ""
+    var age: Int = 0
+    var avgGrade: Double = 0
+    var gender: Int
+    
+    init(fullName: String, studentAge: Int, gender:Int){
+        self.name=fullName
+        self.age=studentAge
+        self.gender=gender
+    }
+    
+    func about(){
+        if (avgGrade==0)
+        {
+            println("\(name) is \(age)")
+        }
+        else
+        {
+        println("\(name) is \(age) and has the average \(avgGrade)")
+        }
+    }
+    
+    // aşağıdaki sistem birşey return ettiği için daha verimli çalışmasını sağlar
+    
+    func desc() -> String {
+        if (avgGrade>0)
+        {
+            return "\(name) is \(age) and has the average \(avgGrade)"
+        }
+        else
+        {
+            return "\(name) is \(age)"
+        }
+    }
+    
+    func getGender() -> String {
+        return GENDER[gender]
+        }
+    }
 
-println("Hello, World!")
+
+var biri = Student(fullName:"Denizer", studentAge:55, gender:1)
+biri.about()
+biri.avgGrade=77
+biri.about()
+
+// daha verimli çalışmasını sağlayan kodun devamı
+println(biri.desc())
+
+
+
+println()
+
+/*
+var y: Int = 80
+for i in 0..<50{
+    y = y+i
+}
+/////////
+y += 1000
+/////////
+
+println(y)
+
+*/
+
+class Girl: Student {
+   // var  gender: Int // ilk değer atanması şart bir yolu şu: = 0 diğeri aşağıdaki init fonksiyonu
+    
+    init(fullName: String, studentAge: Int) {
+        super.init(fullName: fullName, studentAge: studentAge, gender:0)
+    }
+}
+
+var bir_kiz = Girl (fullName: "Sevgi", studentAge: 30)
+bir_kiz.about()
+//println(bir_kiz.gender)
+println((bir_kiz.getGender()))
+println(biri.getGender())
+println("\(biri.name) is \(biri.age) and is a \(biri.getGender())")
+
 
